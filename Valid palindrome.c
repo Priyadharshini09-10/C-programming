311956cb7febf0fe3e6ca411f90e8a6c6a3d5ae8
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-03-04 16:06:57 +0530

    Valid palindrome.c

---
+++ Valid palindrome.c
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindrome(char* s) {
+    
+    int left = 0;
+    int right = strlen(s) - 1;
+    
+    while(left < right) {
+        
+        // Skip non-alphanumeric from left
+        while(left < right && !isalnum(s[left])) {
+            left++;
+        }
+        
+        // Skip non-alphanumeric from right
+        while(left < right && !isalnum(s[right])) {
+            right--;
+        }
+        
+        // Compare lowercase
+        if(tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+}
