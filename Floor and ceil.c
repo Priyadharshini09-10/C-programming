5d4bb598ae039bc9d63f29fcf496dba03a1ec7a5
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-05-22 16:15:39 +0530

    Floor and ceil.c

---
+++ Floor and ceil.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+float main()
+{
+    int a=2,b=3;
+    float x=6.3145,y,k;
+    int c=pow(a,10);
+    printf("%d\n",c);
+    printf("%f\n",y=floor(x));
+    printf("%f\n",k=ceil(x));
+    return 0;
+}
