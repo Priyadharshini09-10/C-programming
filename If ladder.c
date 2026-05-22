0dbe52fda4e9cdbf51d116701e851752ea47a5ab
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-05-22 09:49:10 +0530

    If ladder.c

---
+++ If ladder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the num");
+    scanf("%d",&a);
+    if(a>10)
+    printf("greater than 10\n");
+    if(a>20)
+    printf("greater than 20\n");
+   if(a>30)
+    printf("greater than 10\n");
+    return 0;
+}
