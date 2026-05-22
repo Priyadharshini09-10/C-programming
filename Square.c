246e25a92dcb14f2a115f73ff794540ee0513b59
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-05-22 16:13:21 +0530

    Square.c

--- Square.c
+++ Square.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int sq,i=1,n;
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		sq=i*i;
+		if(i<n)
+			printf("%d,",sq);
+		else
+			printf("%d",sq);
+		i=i+1;
+
+
+	}
+
+	return 0;
+}
 
