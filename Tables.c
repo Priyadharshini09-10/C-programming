be9c96e6792ee3e3483b576ffd199f3649ad99df
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-05-22 09:56:07 +0530

    Tables.c

---
+++ Tables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int m,a;
+    scanf("%d",&m);
+    for(a=1;a<=10;a++)
+    {
+        printf("%d*%d=%d\n",m,a,m*a);
+    }
+       
+        
+    
+    return 0;
+}
